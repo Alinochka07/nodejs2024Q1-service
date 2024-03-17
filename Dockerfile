ceb7cfbf8ef42003702372ca9e19cd415b8eba44
Alinochka07 <alina011988@icloud.com>
2024-03-17 16:58:55 +0600

    feat: modify Dockerfile, compose.yml

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,22 @@ RUN npm install
 
 COPY . .
 
+EXPOSE 4000
+
 RUN npm run build
 
 CMD [ "npm", "start" ]
+
+# Use the official PostgreSQL image from Docker Hub
+FROM postgres:latest
+
+# Set environment variables (optional, you can use .env file)
+ENV POSTGRES_DB=my_database
+ENV POSTGRES_USER=my_user
+ENV POSTGRES_PASSWORD=my_password
+
+# Copy initialization scripts (if any)
+# COPY init.sql /docker-entrypoint-initdb.d/
+
+# Expose PostgreSQL port
+EXPOSE 5432
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AlbumsModule } from './albums/albums.module';
 import { ArtistsModule } from './artists/artists.module';
 import { TracksModule } from './tracks/tracks.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     UsersModule,
     AlbumsModule,
     ArtistsModule,
